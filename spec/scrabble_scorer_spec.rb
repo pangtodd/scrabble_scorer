ae84f511beb0225c74053cc635cc194d01d87a64
Todd Pangilinan <pang.todd@gmail.com>
2022-04-13 17:55:11 -0700

    establish test 1 for word_breaker, split a word into an array of letters.

--- spec/scrabble_scorer_spec.rb
+++ spec/scrabble_scorer_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('scrabble_scorer')
 
 describe(#word_breaker) do
-  it("write stuff here")do
-    expect(stuff()).to eq(morestuff)
+  it("should take a word and return an array with the individual letters")do
+    expect(word_breaker("frog")).to eq(["f","r","o","g"])
   end
 end
