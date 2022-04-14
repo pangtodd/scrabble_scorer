2b39d20101f3212471f84437d0efabcb1e28270d
Todd Pangilinan <pang.todd@gmail.com>
2022-04-13 23:24:32 -0700

    wrote final tests for branching logic

--- spec/scrabble_scorer_spec.rb
+++ spec/scrabble_scorer_spec.rb
@@ -12,9 +12,21 @@ describe ('#sort')do
     expect(sort("k")).to eq(5)
   end
   it('should evaluate a letter and return a numeric value based on the letter- q and z')do
-    expect(sort("z")).to eq(10)
+    expect(sort("x")).to eq(8)
   end
   it('should evaluate a letter and return a numeric value based on the letter- j and x')do
-    expect(sort("x")).to eq(8)
+    expect(sort("z")).to eq(10)
+  end
+  it('should evaluate a letter and return a numeric value based on the letter- b,c,m,p')do
+    expect(sort("p")).to eq(3)
+  end
+  it('should evaluate a letter and return a numeric value based on the letter- f,h,v,w,y')do
+    expect(sort("f")).to eq(4)
+  end
+  it('should evaluate a letter and return a numeric value based on the letter- d and g')do
+    expect(sort("d")).to eq(2)
+  end
+  it('should evaluate a letter and return a numeric value based on the letter- a,e,i,o,u,l,n,r,s,t')do
+    expect(sort("a")).to eq(1)
   end
 end    
