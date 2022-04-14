f673761df1b1ab6ccc298e7851a3e4b73455de89
Todd Pangilinan <pang.todd@gmail.com>
2022-04-13 18:25:59 -0700

    wrote test for sort method, if it is given a k, it should return 8

--- spec/scrabble_scorer_spec.rb
+++ spec/scrabble_scorer_spec.rb
@@ -1,7 +1,7 @@
 require('rspec')
 require('scrabble_scorer')
 
-describe(#word_breaker) do
+describe('#word_breaker')do
   it("should take a word and return an array with the individual letters")do
     expect(word_breaker("frog")).to eq(["f","r","o","g"])
   end
