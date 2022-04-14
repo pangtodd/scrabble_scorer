eccc861dc43ed050a7f7e16c31ad05c08581751d
Todd Pangilinan <pang.todd@gmail.com>
2022-04-13 22:40:29 -0700

    write test 3 for sort method, for j and x

--- spec/scrabble_scorer_spec.rb
+++ spec/scrabble_scorer_spec.rb
@@ -1,15 +1,17 @@
 require('rspec')
 require('scrabble_scorer')
 
-describe('#word_breaker')do
+describe ('#word_breaker') do
   it("should take a word and return an array with the individual letters")do
     expect(word_breaker("frog")).to eq(["f","r","o","g"])
   end
 end
 
-describe('#sort')do
+describe ('#sort')do
   it('should evaluate a letter and return a numeric value based on the letter')do
     expect(sort("k")).to eq(5)
-    expect(sort("q")).to eq(10)
+  end
+  it('should evaluate a letter and return a numeric value based on the letter')do
+    expect(sort("z")).to eq(10)
   end
 end    
